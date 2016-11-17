36c6e9b24361da0165b83981f9a1b73b6585aa18
eumesmo <eumesmamente@riseup.net>
2016-11-17 22:22:24 +0100

    Engadido anima

---
+++ anima.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# coding: utf-8
+'''
+Animación simple. 
+O módulo "os" permite usar comandos do sistema como "clear" para limpar a pantalla.
+O módulo time permite usar "time.sleep(segundos) para parar o programa un tempo"
+Cando poñemos \n facemos un salto de liña. 
+Para poñer algúns símbolos como a barra invertida "\" é necesario poñela dobre "\\"
+polo que para poñer "\o/" teños que facelo poñendo "\\o/" 
+'''
+import os
+import time
+for i in range(5):
+	os.system('clear')
+	print '\n \\o/\n  |\n / \\    S u b e \n'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n  o\n /|\\\n / \\    B a i x a\n'
+	time.sleep(0.3)
+for i in range(4):
+	os.system('clear')
+	print '\n \\o/ \\o/ \\o/ \\o/ \\o/ \\o/'
+	print '  |   |   |   |   |   |'
+	print ' / \\ / \\ / \\ / \\ / \\ / \\    S u b e n'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n  o   o   o   o   o   o'
+	print ' /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print ' / \\ / \\ / \\ / \\ / \\ / \\    B a i x a n'
+	time.sleep(0.3)
+	
+for i in range(1):
+	os.system('clear')
+	print '\n  o   o   o   o   o   o'
+	print ' /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print ' / \\ / \\ / \\ / \\ / \\ / \\    '
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n  o   o   o   o   o   o'
+	print ' /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print ' / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n  o   o   o   o   o   o'
+	print ' /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print ' / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n  o   o   o   o   o   o'
+	print ' /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print ' / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n  o   o   o   o   o   o'
+	print ' /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print ' / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n   o   o   o   o   o   o'
+	print '  /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print '  / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n    o   o   o   o   o   o'
+	print '   /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print '   / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     o   o   o   o   o   o'
+	print '    /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print '    / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     '
+	print '    /|\\ /|\\ /|\\ /|\\ /|\\ /|\\'
+	print '    / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     '
+	print '    /|\\ /|\\ /|\\ '
+	print '    / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     '
+	print '     '
+	print '    / \\ / \\ / \\ / \\ / \\ / \\'
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     '
+	print '     '
+	print '    / \\ / \\ / \\ '
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     '
+	print '     '
+	print '    /  '
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     '
+	print '     '
+	print '    `´  '
+	time.sleep(0.3)
+	os.system('clear')
+	print '\n\n\n\n     '
+	print '     '
+	print '    FIN  '
+	time.sleep(3)
+	os.system('clear')
+	
+	
+	
+	
