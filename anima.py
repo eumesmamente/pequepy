3e0a64c4eb055cbc64bc77f8bb74b797b19b6e46
eumesmo <eumesmamente@riseup.net>
2016-11-17 22:33:28 +0100

    Modificado anima

--- anima.py
+++ anima.py
@@ -10,6 +10,14 @@ polo que para poñer "\o/" teños que facelo poñendo "\\o/"
 '''
 import os
 import time
+'''
+O bucle "for i in range(5) repite 5 veces o que poñamos nel.
+"i" é unha variable que toma o valor do rango en cada volta. O bucle remata cando i chega a 5. 
+"i" pode chamarse de calqueira maneira, por exemplo "e" ou "pfff", da igual.
+O rango (5) equivale a 0,1,2,3,4
+Tamén podemos usar rangos como (3,6) que sería 3,4,5
+Ou (3,11,2) que sería 3,5,7,9
+'''
 for i in range(5):
 	os.system('clear')
 	print '\n \\o/\n  |\n / \\    S u b e \n'
