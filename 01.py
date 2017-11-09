cb583b867a2361ff3e16566b0afe264944e28af5
eumesmo <eumesmamente@riseup.net>
2017-11-09 21:32:43 +0100

    Engadido 01.py

---
+++ 01.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# coding: utf-8
+""" 
+Variables en python:
+Texto e números
+"""
+#Cando unha liña comeza cunha almofada é un comentario.
+#Esta liña é un comentario
+""" Tres comiñas
+permite comentar
+en varias liñas"""
+
+print "print imprime un texto como este na pantalla\n"
+
+# o \n do final da liña anterior é un salto de liña
+
+print """Sempre que poña letras ou números 1 2 3 4 ... 100 
+entre comiñas os números serán como letras ou caracteres.
+Poñendo comiñas triples podo poñer varias liñas\n"""
+
+#Podemos imprimir números e facer operacións con eles
+
+
+print "Sumar 5+4"
+print 5+4
+print "Multiplicar 5x4"
+print 5*4
+print "Dividir 5 entre 4"
+print 5/4
+print "Elevar 5 a 4" 
+print 5**4
+print "Resto da división de 5 entre 4"
+print 5%4
+
+#Tamén podemos usar variables
+
+nada="todo"
+negro="branco"
+
+print nada,negro
+
+algo=5
+print algo
+
+print nada,algo,negro
+
+
+
+
+
+
+
+
+
+
+
+
+
