e2711ebe0a2c0859840edf6f8f3abeb643bb4525
eumesmo <eumesmamente@riseup.net>
2017-12-14 19:44:29 +0100

    Engadido 03.py

---
+++ 03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding: utf-8
+
+print "Pon un número"
+num1=raw_input()
+
+print "Pon outro número"
+num2=raw_input()
+
+num1=int(num1)
+num2=int(num2)
+
+print "Suma:",num1,"+",num2,"=",num1+num2
+
+print "Multiplicación:",num1,"x",num2,"=",num1*num2
+
