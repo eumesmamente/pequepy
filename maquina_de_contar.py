ba83b7b43d33fde9cc1cbd02728c8eec24734464
eumesmo <eumesmamente@riseup.net>
2018-02-14 19:02:56 +0100

    Engadindo maquina de contar

---
+++ maquina_de_contar.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#importamos a función time
+import time
+
+print "Este programa vai contar do 1 ao núñero que poñas 2 veces de dúas formas distintas sempre que non sexa maior de 1000000"
+
+def conta():
+	try:
+		numero=int(raw_input("Pon un número:\n"))
+		#Condicional if
+		if numero>1000000:
+			print "O número é moi grandeeeee!"
+		#En calquera outro caso... (else)
+		else:
+			#Un if dentro dun else... (Unha condicional dentro de outra)
+			if numero>10000 and numero<1000000: 
+				print "O número é bastante grande pero bueno..."
+				#Usamos a función time para que o programa espere 1,5 segundos para continuar...
+				time.sleep(1.5)
+			#Elif é outra condición despois do primeiro if. Podemos poñer os elif que queiramos. 	
+			elif numero<1000000:
+				print " Imprimimos a conta dos números..."
+				#Usamos outra vez a función time...
+				
+			cuenta=0
+			print " Bucle while"
+			time.sleep(1.5)
+			
+			#Este é o bucle while
+			while cuenta<numero: 
+				cuenta=cuenta+1
+				print cuenta,
+				
+			print "\n Bucle for"
+			time.sleep(1.5)
+			
+			#Este é o bucle for	
+			for cuenta in range(1, numero+1):
+				print cuenta,
+			
+	except ValueError:
+		print "E necesario poñer un número\n"		
+
+conta()
